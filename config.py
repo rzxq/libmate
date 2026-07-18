a4932ad2d67f10c4c631478c2a87c38e70019e22
rzxq <egor.kek.2008@gmail.com>
2026-07-18 17:02:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-HF_TOKEN = os.getenv("HF_TOKEN", "")
 DATABASE_PATH = os.getenv("DATABASE_PATH", "bookbot.db")
 
 GOOGLE_BOOKS_API_KEY = os.getenv("GOOGLE_BOOKS_API_KEY", "")
